7af3895c2df402e987e5f1119a349099d139e483
barbie6676 <mian@lexity.com>
2014-11-16 16:09:31 -0800

    Update LIS.java

--- LIS.java
+++ LIS.java
@@ -3,14 +3,16 @@
 // resource: http://en.wikipedia.org/wiki/Longest_increasing_subsequence#Efficient_algorithms 
 // the algorithm will return the length, but you can print out the value from P
 
-public static int longestIncreSubseq(int[] A) {
-		int n = A.length;
+public static int longestIncreSubseq(int[] X) {
+		int n = X.length;
 		
 		if (n<=1) return n;
 		
 		
-		//M[j] store the position k that smallest of value X[k] that length of j lis can end at X[k]
-		//for example, a sequence of length 4 lis can end at X[5] = 3, or at X[9] = 4, M[4] = 9
+		//M[j] stores the index k of the smallest value X[k] such that there is an increasing subsequence of length j ending at X[k]
+		//for example, X = [0, 8, 4, 12, 2, 10, 6, 14, 1, 9, 5, 13, 3, 11, 7, 15]
+		// j = 1, M[1] = 0; j = 2, M[2] = 1 (0,8); j = 3, M[3] = 3 (X[2] = 12, 0,8,12, or 0,4,12)
+		// j = 4, an increasing subsequence of len 4 can end at X[7] (0,8,10,14), or at X[9](0,4,6,9) the smaller k is 7, M[4] = 7
 		//P[k] stores the position of the predecessor of X[k] in the lis ending at X[k]
 		// for example 0 4 5 is a lis, P[2] = 1, because X[2] = 5, the predecessor is 4, position is 1
 		
@@ -27,7 +29,7 @@ public static int longestIncreSubseq(int[] A) {
 			 int r = L;
 			 while (l<=r) {
 				 int m = (l+r)/2;
-				 if (A[M[m]]<A[i]) {
+				 if (X[M[m]]<X[i]) {
 					 l = m+1;
 				 } else {
 					 r = m-1;
@@ -37,18 +39,18 @@ public static int longestIncreSubseq(int[] A) {
 			 
 			 P[i] = M[j];
 			 			 
-		   if (j == L || A[i] < A[M[j+1]]){
+		   if (j == L || X[i] < X[M[j+1]]){
 		       M[j+1] = i;
 		       L = Math.max(L, j+1);
 		    }
 		 }
 		 
-		 //Methods.printArray(A);
+		 //Methods.printArray(X);
 		 //Methods.printArray(M);
 		 //Methods.printArray(P);
 		 int j = M[L];
 		 while (j!=-1) {
-			 System.out.print(A[j]+" ");
+			 System.out.print(X[j]+" ");
 			 j = P[j];
 		 }		       
 		       
